53a9a749aa216806203581c825ad44520de3d093
Yuxuan Zhang <zhangyuxuan171@mails.ucas.ac.cn>
2019-06-17 20:14:26 +0800

    manually commit

---
+++ Elevator/Elevator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Elevator
+//
+//  Created by 张宇轩 on 2019/6/17.
+//  Copyright © 2019 张宇轩. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
